7cc6314f0a7708c8efebc223068730faedc8109e
musfirotus <musfirotus@gmail.com>
2020-08-26 22:52:56 +0700

    tampil data post

--- src/pages/Posts/Data.vue
+++ src/pages/Posts/Data.vue
@@ -1,16 +1,15 @@
 <template>
-  <div>
-    Post Pages
-    {{ message }}
-
-    <ul>
-      <li v-for="(post, i) in postList" :key="i">
-        <router-link :to="'/post/'+post.id">
-          {{ post.title }} - {{ post.id }}
-        </router-link>
-      </li>
-    </ul>
-  </div>
+    <div class="max-w-sm w-full lg:max-w-full lg:flex">
+      <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div v-for="(post, i) in postList" :key="i" class="mb-8">
+          <div class="text-gray-900 font-bold text-xl mb-2">{{ post.title }}</div>
+          <p class="text-gray-700 text-base">{{ post.body }}</p>
+          <router-link class="flex items-center text-red-700" :to="'/post/'+post.id">
+            Read More
+          </router-link>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -26,7 +25,6 @@ export default {
   },
   created() {
     console.log('Post created.');
-    // this.postList = posts;
   }
 }
 </script>
