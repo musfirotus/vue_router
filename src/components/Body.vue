445659355e345994e280e29b479c626ee3ecfb77
musfirotus <musfirotus@gmail.com>
2020-08-26 22:53:56 +0700

    Body

---
+++ src/components/Body.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="bg-white text-black text-2xl p-8">
+    <router-view></router-view>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: 'Footer'
+  }
+</script>
