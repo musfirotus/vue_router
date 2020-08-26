b43439c7d1c83117cda0086d2e264b3d165b9632
musfirotus <musfirotus@gmail.com>
2020-08-27 00:12:15 +0700

    Layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <div id="app" class="flex flex-col h-screen justify-between">
+  <div id="app" class="flex flex-wrap h-screen justify-between">
     <Header />
-    <SideBar />
-    <Widget />
     <Body />
+    <Widget />
     <Footer />
-    
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-import SideBar from './components/SideBar.vue'
-import Widget from './components/Widget.vue'
 import Body from './components/Body.vue'
 import Footer from './components/Footer.vue'
 
@@ -20,8 +16,6 @@ export default {
   name: 'App',
   components: {
     Header,
-    SideBar,
-    Widget,
     Body,
     Footer
   }
