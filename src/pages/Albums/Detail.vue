058b06ae129e0c031ce0b238a2113ac920ba34aa
musfirotus <musfirotus@gmail.com>
2020-08-27 08:45:20 +0700

    detail album

--- src/pages/Albums/Detail.vue
+++ src/pages/Albums/Detail.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    Album Page
-    <!-- {{ $route.params.id }} -->
+    <div class="mx-auto max-w-screen-lg h-full rounded shadow-lg">
+      <div v-for="album in albumList" :key="album.id" :list="album" class="max-w rounded border border-gray-300 shadow-lg pt-5">
+        <h1 class="font-semibold text-center text-3xl">{{album.title}}</h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,13 +15,14 @@ export default {
   name: 'AlbumDetail',
   data() {
     return {
-      albumList: albums,
-      message: "halo"
+      albumList: [],
     }
   },
   created() {
     console.log('Album created.');
-    // this.albumList = albums
+    return this.albumList = albums.filter(val => {
+      return val.id == this.$route.params.id
+    })
   }
 }
 </script>
--- src/pages/Posts/Detail.vue
+++ src/pages/Posts/Detail.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="mx-auto max-w-screen-lg h-full rounded overflow-hidden shadow-lg">
-      <div v-for="post in postData" :key="post.id" :list="post" class="max-w rounded border border-gray-300 shadow-lg pt-5">
+    <div class="mx-auto max-w-screen-lg h-full rounded shadow-lg">
+      <div v-for="post in postList" :key="post.id" :list="post" class="max-w rounded border border-gray-300 shadow-lg pt-5">
         <h1 class="font-semibold text-center text-3xl">{{post.title}}</h1>
         <h5 class="px-12 pb-12 pt-6">{{post.body}}</h5>
       </div>
@@ -16,13 +16,13 @@ export default {
   name: 'PostDetail',
   data() {
     return {
-      postData: []
+      postList: []
     }
   },
   created() {
-      return this.postData = posts.filter(post => {
-          return post.id == this.$route.params.id
-      })
+    return this.postList = posts.filter(val => {
+      return val.id == this.$route.params.id
+    })
   }
 }
 </script>
